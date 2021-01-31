4333960902a4247704da7a7e084840a19f3bf1fa
Rubricalkarma <syrusbomberger@yahoo.com>
2021-01-31 01:41:06 -0600

    Added Lobby

---
+++ CoderRoyale/CoderRoyale/Hubs/Lobby.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoderRoyale.Hubs
+{
+    public static class Lobby
+    {
+        public static bool inLobby = true;
+        public static Dictionary<string, string> Players = new Dictionary<string, string>();
+    }
+}
