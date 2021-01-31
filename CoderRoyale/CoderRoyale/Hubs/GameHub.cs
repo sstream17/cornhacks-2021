bf01e4bbddd9aa98f2af9ad07fba2b23fc1cdeb3
Spencer Stream <sprewstream@gmail.com>
2021-01-30 20:37:19 -0600

    Setting up Client connections to SignalR

--- CoderRoyale/CoderRoyale/Hubs/GameHub.cs
+++ CoderRoyale/CoderRoyale/Hubs/GameHub.cs
@@ -5,9 +5,39 @@ namespace CoderRoyale.Hubs
 {
 	public class GameHub : Hub
 	{
-		public async Task SendMessage(string user, string message)
+		public async Task SendExecutionResults(
+			string submittedUser,
+			int timeSubmitted,
+			string output)
 		{
-			await Clients.All.SendAsync("ReceiveMessage", user, message);
+			await Clients.All.SendAsync(
+				"ReceiveExecutionResults",
+				submittedUser,
+				timeSubmitted,
+				output);
+		}
+
+		public async Task SendPlayerComplete(string successfulPlayer)
+		{
+			await Clients.All.SendAsync("ReceivePlayerComplete", successfulPlayer);
+		}
+
+		public async Task SendPlayerDisconnect(string disconnectedPlayer)
+		{
+			await Clients.All.SendAsync("ReceivePlayerDisconnect", disconnectedPlayer);
+		}
+
+		public async Task SendRemainingTime(int remainingTime)
+		{
+			await Clients.All.SendAsync("ReceiveRemainingTime", remainingTime);
+		}
+
+		public async Task SendNextRound(int roundNumber, int numberOfPlayersToAdvance)
+		{
+			await Clients.All.SendAsync(
+				"ReceiveNextRound",
+				roundNumber,
+				numberOfPlayersToAdvance);
 		}
 	}
 }
