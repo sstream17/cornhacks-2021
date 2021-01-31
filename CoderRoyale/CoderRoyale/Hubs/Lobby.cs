d7229e53d489e539912d322995021881c519f97e
Spencer Stream <sprewstream@gmail.com>
2021-01-31 08:29:43 -0600

    Fixing crash when five minute timer would finish

--- CoderRoyale/CoderRoyale/Hubs/Lobby.cs
+++ CoderRoyale/CoderRoyale/Hubs/Lobby.cs
@@ -1,4 +1,6 @@
 ﻿using System.Collections.Generic;
+using System.Timers;
+using Microsoft.AspNetCore.SignalR.Client;
 
 namespace CoderRoyale.Hubs
 {
@@ -6,5 +8,28 @@ namespace CoderRoyale.Hubs
 	{
 		public static bool inLobby = true;
 		public static Dictionary<string, string> Players = new Dictionary<string, string>();
+
+		private static Timer timer;
+
+		private static HubConnection connection = new HubConnectionBuilder()
+				.WithUrl("https://localhost:44316/gamehub")
+				.Build();
+
+		public async static void SetTimer(int seconds)
+		{
+			if (!connection.State.Equals(HubConnectionState.Connected))
+			{
+				await connection.StartAsync();
+			}
+
+			timer = new Timer(seconds * 1000);
+			timer.Elapsed += OnTimer;
+			timer.Enabled = true;
+		}
+
+		private async static void OnTimer(object source, ElapsedEventArgs e)
+		{
+			await connection.InvokeAsync("SendEndGame");
+		}
 	}
 }
